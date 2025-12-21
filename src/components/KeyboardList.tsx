efde2f04b4d2c69219adec1034db8bd35281530e
Brab <brandon.tossan@gmail.com>
2025-12-20 19:45:30 -0500

    initial commit with rough layout

---
+++ src/components/KeyboardList.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const KeyboardList: React.FC = () => {
+  const [keyboards, setKeyboards] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/keyboards/")
+    .then(response => {
+      setKeyboards(response.data);
+    })
+    .catch(error => {
+      console.error("Error fetching products", error);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Newly Arrived Keyboards</h2>
+      <div>
+        {keyboards.map((keyboard) => (
+          <div key={keyboard.id}>
+            <h3>{keyboard.name}</h3>
+            <img src={keyboard.image} alt={keyboard.name} />
+            <p>{keyboard.description}</p>
+            <span>${keyboard.price}</span>
+            <button>Add to Cart</button>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default KeyboardList;
